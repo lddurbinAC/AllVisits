7817097dc1d3879dd301d6c6b4ffc34d2c439f7c
Lee Durbin <l.d.durbin@gmail.com>
2021-11-04 03:08:34 +0000

    Pull data straight from Google Sheets, but comment out for now

--- scripts/AllVisits_AandR_prep.R
+++ scripts/AllVisits_AandR_prep.R
@@ -8,3 +8,21 @@ AandR_prep <- function(x, id) {
 # Get Records and Archives email enquiries and website sessions
 AandR_prep(here::here("data/raw/AandR_enquiries"), id = 1) %>% saveRDS(here::here("data/rds/AandR_enquiries.rds"))
 AandR_prep(here::here("data/raw/AandR_sessions"), id = 2) %>% saveRDS(here::here("data/rds/AandR_sessions.rds"))
+
+# library(googlesheets4, warn.conflicts = FALSE)
+# 
+# gs4_deauth()
+# 
+# read_sheet(
+#   "https://docs.google.com/spreadsheets/d/1NFj65L8iCrCQJg0ABJq-onsm9Wb0IZR_WqbF84pN5jk/edit#gid=697526476",
+#   range = "B2:H"
+#   ) %>% 
+#   clean_names() %>% 
+#   select(month, starts_with("sessions")) %>% 
+#   mutate(
+#     year = year(month),
+#     month = month(month, label = TRUE)
+#     ) %>% 
+#   rowwise() %>% 
+#   mutate(metric = sum(across(starts_with("sessions")), na.rm = TRUE), .keep = "unused") %>% 
+#   ungroup()
